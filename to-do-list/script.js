69162f2c29ec5c690b4b613f135df3e8fb02f055
Gabe-Morais <gabe-morais@outlook.com>
2022-07-28 17:34:03 -0300

    To-do List

---
+++ to-do-list/script.js
@@ -0,0 +1,4 @@
+const texto = document.querySelector('input')
+const btnInsert = document.querySelector('.divInsert button')
+const btnDeleteAll = document.querySelector('.header button')
+const ul =
