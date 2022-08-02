17418e278eda23de6f45fa178200ffb6288f5849
Gabriel Morais <gabe-morais@outlook.com>
2022-08-01 21:39:12 -0300

    .

--- to-do-list/script.js
+++
@@ -1,4 +0,0 @@
-const texto = document.querySelector('input')
-const btnInsert = document.querySelector('.divInsert button')
-const btnDeleteAll = document.querySelector('.header button')
-const ul =
